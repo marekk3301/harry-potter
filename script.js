4b8e55b856199db78b3248659b77a6acbe5c4a46
Marek <marekk3301@gmail.com>
2022-10-09 02:57:59 +0200

    more js work
    data is displayed
    sorting mostly works
    modal opens and closes

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
 const api = "https://hp-api.herokuapp.com/api/characters/";
 const table = document.getElementById("students");
-table.border = "1px";
-const columns = ["name", "dateOfBirth", "house", "wizard", "ancestry", "hogwartsStudent"];
 const category = "students";
+let studentData = null;
+let sortMode = null;
 
-async function populateTable(url, table, category) {
-    const tableHead = table.querySelector("thead");
-    const tableBody = table.querySelector("tbody");
+const title = document.getElementById("students__title");
 
+async function fetchData(category) {
+    let url = api;
     url += category;
 
     // using await bc fetch and json are asynchronous and we need to wait for the api response
     const response = await fetch(url);
     const studentData = await response.json();
+
+    return studentData;
+}
+
+async function renderTable(category, sortBy) {
+    const table = document.getElementById("students");
+    let tableBody = table.querySelector("tbody");
+
+    if (!tableBody) {
+        tableBody = document.createElement("tbody");
+        table.appendChild(tableBody);
+    }
+
+    tableBody.innerHTML = "";
+
+    if (category !== null) {
+        // fetch new data from api 
+        studentData = await fetchData(category);
+    } else if (sortBy === "name") {
+        studentData.sort(function(a, b) {
+            if (sortMode === 'asc') {
+                return ('' + a.name).localeCompare(b.name);
+            }
+            return ('' + b.name).localeCompare(a.name);
+        });
+    } else if (sortBy === "dateOfBirth") {
+        studentData.sort(function(a, b) {
+            // definitely not a way to sort dates :D
+            if (sortMode === 'asc') {
+                return ('' + a.dateOfBirth).localeCompare(b.dateOfBirth);
+            }
+            return ('' + b.dateOfBirth).localeCompare(a.dateOfBirth);
+        });
+    } else if (sortBy === "house") {
+        studentData.sort(function(a, b) {
+            if (sortMode === 'asc') {
+                return ('' + a.house).localeCompare(b.house);
+            }
+            return ('' + b.house).localeCompare(a.house);
+        });
+    }
+
+    // console.log({ studentData })
 
     for (let i = 0; i < studentData.length; i++) {
         const row = document.createElement("tr");
@@ -23,39 +66,79 @@ async function populateTable(url, table, category) {
         // }
 
         const nameCell = document.createElement("td");
-        nameCell.innerHTML = studentData[i].name;
+        nameCell.textContent = studentData[i].name;
         row.appendChild(nameCell);
 
         const dateCell = document.createElement("td");
-        dateCell.innerHTML = studentData[i].dateOfBirth;
+        dateCell.textContent = studentData[i].dateOfBirth;
         row.appendChild(dateCell);
 
         const houseCell = document.createElement("td");
-        houseCell.innerHTML = studentData[i].house;
+        houseCell.textContent = studentData[i].house;
         row.appendChild(houseCell);
 
         const wizardCell = document.createElement("td");
-        wizardCell.innerHTML = studentData[i].wizard;
+        wizardCell.textContent = studentData[i].wizard;
         row.appendChild(wizardCell);
 
         const ancestryCell = document.createElement("td");
-        ancestryCell.innerHTML = studentData[i].ancestry;
+        ancestryCell.textContent = studentData[i].ancestry;
         row.appendChild(ancestryCell);
 
         const studentCell = document.createElement("td");
         if (studentData[i].hogwartsStudent === true) {
-            studentCell.innerHTML = "student";
+            studentCell.textContent = "student";
         } else if (studentData[i].hogwartsStaff === true) {
-            studentCell.innerHTML = "staff";
+            studentCell.textContent = "staff";
         } else {
-            studentCell.innerHTML = "other";
+            studentCell.textContent = "other";
         }
         row.appendChild(studentCell);
+
+        row.addEventListener('click', function() {
+            openCharacterModal(studentData[i]);
+        })
 
         tableBody.appendChild(row);
     }
 }
 
+function sortTable(col) {
+    if (sortMode === null || sortMode === "desc") {
+        sortMode = 'asc';
+    } else if (sortMode === 'asc') {
+        sortMode = 'desc';
+    }
+
+    renderTable(null, col);
+}
+
+function openCharacterModal(character) {
+    const modal = document.getElementById("modal");
+    const modalImg = modal.querySelector("img");
+    const modalName = document.getElementById("modal__name");
+    const modalInfoList = document.getElementById("modal__info");
+    const characterInfo = ["actor", "ancestry", "dateOfBirth", "eyeColour", "gender", "hairColour", "house", "patronus", "species", "wand"];
+
+    modalImg.src = character.image;
+    modalName.innerHTML = character.name;
+
+    // cant get attribute from string
+    for (info of characterInfo) {
+        console.log(character.info);
+    }
+
+    modal.style.display = 'block';
+}
+
+function closeCharacterModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = 'none';
+}
+
+renderTable(category);
 
 
-populateTable(api, table, category)
+// sortowanie dat
+// wyświetlanie danych w modalu
+//
